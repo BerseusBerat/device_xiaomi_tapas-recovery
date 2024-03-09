f282dc8abcf470819a0f7f4461d9c49b9d0ea39f
Berat <75970161+BerseusBerat@users.noreply.github.com>
2024-03-09 19:36:44 +0300

    Update and rename twrp_tapas.mk to omni_tapas.mk
    Signed-off-by: Berat <75970161+BerseusBerat@users.noreply.github.com>

--- omni_tapas.mk
+++ omni_tapas.mk
@@ -2,7 +2,12 @@
 # Copyright (C) 2023 The Android Open Source Project
 #
 # SPDX-License-Identifier: Apache-2.0
-#
+
+
+
+# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+
 
 # Configure base.mk
 $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
@@ -14,12 +19,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from device configuration
 $(call inherit-product, device/xiaomi/tapas/device.mk)
 
-# Inherit from TWRP common configuration
-$(call inherit-product, vendor/twrp/config/common.mk)
+# Inherit from pbrp common configuration
+$(call inherit-product, vendor/pb/config/common.mk)
 
 ## Device identifier
 PRODUCT_DEVICE := tapas
-PRODUCT_NAME := twrp_tapas
+PRODUCT_NAME := omni_tapas
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 12 4G
 PRODUCT_MANUFACTURER := Xiaomi
